6451f07ca9467ea2d30f2e81c1ce2a79cf9e1d9e
Александр <bigboa@yandex.ru>
2021-11-11 06:58:24 +0700

    Добавление методов для разных видов подсчетов, добавление форматирования к выводу

--- main.py
+++ main.py
@@ -11,9 +11,11 @@ def get_number_char(char: str) -> int:
     return 0
 
 
+# Создание матрицы для расчетов
 def build_matrix(text: str):
-    array_strings = text.lower().split()
-    array_strings.append(text)
+    lower_text = text.lower()
+    array_strings = lower_text.split()
+    array_strings.append(lower_text)
     count_strings = len(array_strings)
     mass_result = [[0 for j in range(5)] for i in range(count_strings)]
 
@@ -23,14 +25,35 @@ def build_matrix(text: str):
     return mass_result
 
 
+# Подсчет числа по правилу одного
 def calc_one_number(num: int) -> int:
-    return 0
+    if num < 10:
+        return num
+
+    while num > 9:
+        result = 0
+        for number in str(num):
+            result = result + int(number)
+        num = result
+
+    return num
 
 
+# Подсчет числа по правилу Таро
 def calc_taro_number(num: int) -> int:
-    return 0
+    if num < 22:
+        return num
+
+    while num > 21:
+        result = 0
+        for number in str(num):
+            result = result + int(number)
+        num = result
+
+    return num
 
 
+# Подсчет чисел для матрицы
 def calc_sum_char(input_matrix):
     for i in range(len(input_matrix)):
         input_matrix[i][0] = len(input_matrix[i][4])
@@ -42,10 +65,19 @@ def calc_sum_char(input_matrix):
         input_matrix[i][3] = calc_one_number(sum_char)
 
 
-if __name__ == '__main__':
-    inputText = input("Введите предложение: ")
-    matrix_result = build_matrix(inputText)
-    calc_sum_char(matrix_result)
-    sumResult: int = 0
+def print_matrix(matrix):
+    header_strings = ['Число букв', 'Общая сумма', 'По правилу Таро', 'По правилу одного', 'Строка']
+    print("{:═>12}{:═>14}{:═>18}{:═>20}{:═>20}".format('╦', '╦', '╦', '╦', ''))
+    print("{:<10s} ║ {:<11s} ║ {:<15s} ║ {:<17s} ║ {:s}".format(*header_strings))
+    print("{:═>12}{:═>14}{:═>18}{:═>20}{:═>20}".format('╬', '╬', '╬', '╬', ''))
+    for i in range(len(matrix)):
+        print("{:^10d} ║ {:^11d} ║ {:^15d} ║ {:^17d} ║ {:s}".format(*matrix[i]))
 
-    print("Вывод")
+
+if __name__ == '__main__':
+    input_text = input("Введите предложение: ")
+    matrix_result = build_matrix(input_text)
+    calc_sum_char(matrix_result)
+    sum_result: int = 0
+
+    print_matrix(matrix_result)
